727b5d9ddf0cb4b603f3cbacb3d6ce5627d47857
v1n3gAr <severyanov-andrei@yandex.ru>
2022-03-30 01:46:22 +0300

    Added telegram send msg

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 from openpyxl import Workbook
 from datetime import datetime
 import time
+import telegram_send
 
 
-class Parser:
+
+class Parser():
     def __init__(self, text: str):
         self.text = text  # Запрос, который мы будем передавать для поиска на hh.ru
         self._area = 66  # Код города Нижний Новгород, все id городов находятся в документации к API
@@ -29,6 +31,7 @@ class Excel:
         # чтобы каждый новый файл был уникален
         self.wb = Workbook()
         self.ws = self.wb.active
+        self.file_name_xlsx = f"Парсинг за {self.date_and_time}.xlsx"
         headings = {
             'A1': 'Наименование вакансии',
             'B1': 'Зарплата',
@@ -61,11 +64,27 @@ class Excel:
             pass
 
     def save(self):
-        self.wb.save(f"Парсинг за {self.date_and_time}.xlsx")
+        self.wb.save(self.file_name_xlsx)
+        return self.file_name_xlsx
+
 
 
     def close(self):
         self.wb.close()
+
+
+class Telegram():
+    def send_file(self, filename):
+        self.data = {
+            'caption': 'Результат парсинга',
+            'chat_id': '946919713',
+            'url': 'https://api.telegram.org/bot5239048826:AAHYeNnl1b5NaGlbSkqw84w75xUCVafkr_M/sendDocument'
+        }
+        with open(filename, 'rb') as f:
+            files = {'document': f}
+            requests.post(self.data['url'], data=self.data, files=files)
+
+
 
 
 class Robot:
@@ -79,9 +98,14 @@ class Robot:
         data_from_parsing = parser.parsing()
         excel = Excel(self.date_and_time)
         excel.fill_in_requirement(data_from_parsing)
-        excel.save()
+        file_name = excel.save()
+        excel.close()
+        telegram = Telegram()
+        telegram.send_file(filename=file_name)
 
 
 if __name__ == '__main__':
     andrew = Robot('Python Developer')
     andrew.start()
+
+
